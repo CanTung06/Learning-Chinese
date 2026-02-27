cd524d4b52d789601fb41cf16732fbecb874119e
CanTung <cantung678@gmail.com>
2026-02-27 20:15:59 +0700

    Thêm trang đăng nhập

--- js/auth.js
+++ js/auth.js
@@ -5,20 +5,27 @@ import {
     signOut
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-window.login = async function(email, password) {
-    await signInWithEmailAndPassword(auth, email, password);
+window.login = async function() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        window.location.href = "index.html";
+    } catch (e) {
+        alert("Sai tài khoản!");
+    }
 };
 
 window.logout = async function () {
     await signOut(auth);
-    window.location.href = "index.html";
+    window.location.href = "login.html";
 };
 
 export function checkAuth(callback) {
     onAuthStateChanged(auth, user => {
         if (!user) {
-            alert("Chưa đăng nhập!");
-            window.location.href = "index.html";
+            window.location.href = "login.html";
         } else {
             callback();
         }
