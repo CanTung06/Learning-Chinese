3b3e2a0907253538da8ceadbf9c3e466222100a4
Can Tung <cantung678@gmail.com>
2026-02-27 11:39:41 +0700

    Fix lỗi

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCuMXMLPDIRG_Spnf1yD0-x1mzdh6cv5uk",
-    authDomain: "learning-chinese-a88ac.firebaseapp.com",
-    projectId: "learning-chinese-a88ac",
-    storageBucket: "learning-chinese-a88ac.firebasestorage.app",
-    messagingSenderId: "695922961177",
-    appId: "1:695922961177:web:11d4b2c3a3a068f1bdb388",
-    measurementId: "G-MN7THEXCBQ"
+  apiKey: "AIzaSyCuMXMLPDIRG_Spnf1yD0-x1mzdh6cv5uk",
+  authDomain: "learning-chinese-a88ac.firebaseapp.com",
+  projectId: "learning-chinese-a88ac",
+  storageBucket: "learning-chinese-a88ac.firebasestorage.app",
+  messagingSenderId: "695922961177",
+  appId: "1:695922961177:web:11d4b2c3a3a068f1bdb388",
+  measurementId: "G-MN7THEXCBQ"
 };
 
 const app = initializeApp(firebaseConfig);
